9cdcabce7fd29e7aab22adcdcd7d990511701fb7
Adrian <adrianantico@gmail.com>
2018-10-08 10:32:00 -0700

    Update Adrian_Antico_Modeling_Tools_Library.R

--- R/Adrian_Antico_Modeling_Tools_Library.R
+++ R/Adrian_Antico_Modeling_Tools_Library.R
@@ -1486,7 +1486,7 @@ FAST_GDL_Feature_Engineering <- function(data,
   # Max data to keep
   MAX_RECORDS_FULL <- max(max(lags+1),max(periods*2),RecordsKeep)
   MAX_RECORDS_LAGS <- max(max(lags+1),RecordsKeep)
-  MAX_RECORDS_ROLL <- max(max(periods+1),RecordsKeep)
+  MAX_RECORDS_ROLL <- max(max(periods*2),RecordsKeep)
 
   # Set up counter for countdown
   CounterIndicator = 0
@@ -3105,66 +3105,4 @@ Word2VecModel <- function(datax,
     h2o.shutdown(prompt = FALSE)
   }
   return(data)
-}
-
-#' Fast rolling mean
-#' @param DT	The data.table to operate on
-#' @param col A quoted column name
-#' @param N An integer number that we want to shift by
-#' @param Name (optional) the name of the new column
-#' @param by (optional) A quoted by parameter
-#' @param partial (boolean) - if you want a partial mean
-#' @param na.rm (boolean) - if you want a ignore NAs (this will run slightly slower)
-#' @param ... Some additional parameters that you can pass to the \code{\link{shift}} function
-#' @return The modified data.table with the new shifted columns
-#' @export
-#' @examples
-#' set.seed(123)
-#' DT <- data.table(x = sample(10), y = sample(1:2, 10, replace = TRUE), key = "y")
-#' frollmean(DT, "x", 3, by = "y", type = "lead")
-
-frollmean <- function(DT, col, N, Name, by, partial = FALSE, na.rm = FALSE, ...){
-
-  if(missing(Name)) new_col <- paste0(col, "_mean_", N)  else new_col <- Name
-
-  if(missing(by)) {
-
-    if(na.rm) {
-
-      DT[, (new_col) := rowMeans(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...)), na.rm = TRUE)]
-
-      if(!partial) DT[1L:(N - 1L), (new_col) := NA_real_]
-
-    } else if(partial) {
-
-      DT[, (new_col) := as.numeric(Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), fill = 0, ...)))]
-      indx <- seq_len(N)
-      DT[indx, (new_col) := eval(as.name(new_col)) / indx]
-      DT[-indx, (new_col) := eval(as.name(new_col)) / N]
-
-    } else DT[, (new_col) := Reduce(`+`, shift(eval(as.name(col)), 0L:(N - 1L), ...)) / N]
-
-  } else {
-
-    if(na.rm){
-
-      DT[, (new_col) := rowMeans(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...)), na.rm = TRUE), by = by]
-
-      if(!partial) {
-
-        indx <- seq_len(N - 1L)
-        DT[, (new_col) := replace(eval(as.name(new_col)), indx, NA_real_), by = by]
-
-      }
-
-    } else if(partial) {
-
-      DT[, (new_col) := as.numeric(Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), fill = 0, ...))), by = by]
-      indx <- seq_len(N)
-      DT[, (new_col) := c(eval(as.name(new_col))[indx] / indx, eval(as.name(new_col))[-indx] / N), by = by]
-
-    } else DT[, (new_col) := Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), ...)) / N, by = by]
-
-  }
-
 }
