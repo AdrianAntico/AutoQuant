d0e15d4fcaccd94c1015999e921013a6ee981111
Adrian <adrianantico@gmail.com>
2019-05-07 08:39:14 -0700

    Update RemixAutoML.R

--- R/RemixAutoML.R
+++ R/RemixAutoML.R
@@ -220,29 +220,29 @@ ProblematicFeatures <- function(data,
                                 NA_Rate = 0.20,
                                 Zero_Rate = 0.20,
                                 HighSkewThresh = 10) {
-
+  
   # Require data.table
   requireNamespace('data.table', quietly = FALSE)
-
+  
   # Convert to data.table----
   if(!data.table::is.data.table(data)) data <- data.table::as.data.table(data)
-
+  
   # Subset columns of interest----
   keep <- names(data)[ColumnNumbers]
   data <- data[, ..keep]
-
+  
   # Define Functions for Calculations----
   LowVarianceFeatures <- function(data, NearZeroVarThresh = 0.05) {
-
+    
     # Skip Option----
     if(is.null(NearZeroVarThresh)) return(NA)
-
+    
     # Ensure argument is valid----
     if(NearZeroVarThresh > 1) warning("NearZeroVarThresh should be between zero and one")
-
+    
     # Get Row Count----
     xx <- data[, .N]
-
+    
     # Begin process----
     NumNearZeroVariance <- list()
     for (i in seq_len(ncol(data))) {
@@ -250,8 +250,8 @@ ProblematicFeatures <- function(data,
         NumNearZeroVariance[names(data)[i]] <- round(length(unique(data[[i]])) / xx,4)
       }
     }
-
-    if(exists("NumNearZeroVariance")) {
+    
+    if(length(NumNearZeroVariance) > 0) {
       a <- tryCatch({data.table::as.data.table(data.table::melt(NumNearZeroVariance))},
                     error = function(x) NA)
       if(dim(a)[1] != 0) {
@@ -266,16 +266,16 @@ ProblematicFeatures <- function(data,
     }
   }
   HighCardinalityFeatures <- function(data, CharUniqThresh = 0.50) {
-
+    
     # Skip Option----
     if(is.null(CharUniqThresh)) return(NA)
-
+    
     # Ensure argument is valid----
     if(CharUniqThresh > 1) warning("CharUniqThresh should be between zero and one")
-
+    
     # Get Row Count----
     xx <- data[, .N]
-
+    
     # Begin process----
     CharUniqueTooHigh <- list()
     for (i in seq_len(ncol(data))) {
@@ -283,7 +283,7 @@ ProblematicFeatures <- function(data,
         CharUniqueTooHigh[names(data)[i]] <- round(length(unique(data[[i]])) / xx,4)
       }
     }
-    if(exists("CharUniqueTooHigh")) {
+    if(length(CharUniqueTooHigh) > 0) {
       a <- tryCatch({data.table::as.data.table(data.table::melt(CharUniqueTooHigh))},
                     error = function(x) NA)
       if(dim(a)[1] != 0) {
@@ -298,16 +298,16 @@ ProblematicFeatures <- function(data,
     }
   }
   HighMissingCountFeatures <- function(data, NA_Rate = 0.20) {
-
+    
     # Skip Option----
     if(is.null(NA_Rate)) return(NA)
-
+    
     # Ensure argument is valid----
     if(NA_Rate > 1) warning("HighSkewThresh should be between zero and one")
-
+    
     # Get Row Count----
     xx <- data[, .N]
-
+    
     # Begin process----
     LargeNAs <- list()
     for (i in seq_len(ncol(data))) {
@@ -315,7 +315,7 @@ ProblematicFeatures <- function(data,
         LargeNAs[names(data)[i]] <- round(sum(is.na(data[[i]])) / xx,4)
       }
     }
-    if(exists("LargeNAs")) {
+    if(length(LargeNAs) > 0) {
       a <- tryCatch({data.table::as.data.table(data.table::melt(LargeNAs))},
                     error = function(x) NA)
       if(dim(a)[1] != 0) {
@@ -330,13 +330,13 @@ ProblematicFeatures <- function(data,
     }
   }
   HighZeroCountFeatures <- function(data, Zero_Rate = 0.20) {
-
+    
     # Skip Option----
     if(is.null(Zero_Rate)) return(NA)
-
+    
     # Get Row Count----
     xx <- data[, .N]
-
+    
     # Begin process----
     LargeZeros <- list()
     for (i in seq_len(ncol(data))) {
@@ -344,7 +344,7 @@ ProblematicFeatures <- function(data,
         LargeZeros[names(data)[i]] <- round(data[get(names(data)[i]) == 0, .N] / xx,4)
       }
     }
-    if(exists("LargeZeros")) {
+    if(length(LargeZeros) > 0) {
       a <- tryCatch({data.table::as.data.table(data.table::melt(LargeZeros))},
                     error = function(x) NA)
       if(dim(a)[1] != 0) {
@@ -359,18 +359,18 @@ ProblematicFeatures <- function(data,
     }
   }
   HighSkewFeatures <- function(data, HighSkewThresh = 10) {
-
+    
     # Skip Option----
     if(is.null(HighSkewThresh)) return(NA)
-
+    
     # Ensure argument is valid----
     if(!is.numeric(HighSkewThresh) & !is.integer(HighSkewThresh)) {
       warning("HighSkewThresh should a numeric value")
     }
-
+    
     # Get Row Count----
     xx <- data[, .N]
-
+    
     # Begin process----
     HighSkew <- list()
     for (i in seq_len(ncol(data))) {
@@ -386,7 +386,7 @@ ProblematicFeatures <- function(data,
         }
       }
     }
-    if(exists("HighSkew")) {
+    if(length(HighSkew) > 0) {
       a <- tryCatch({data.table::as.data.table(data.table::melt(HighSkew))},
                     error = function(x) NA)
       if(dim(a)[1] != 0) {
@@ -400,51 +400,51 @@ ProblematicFeatures <- function(data,
       return(NA)
     }
   }
-
+  
   # Initalize collection
   collect <- list()
   z <- 0
-
+  
   # LowVarianceFeatures Run----
   a <- tryCatch({LowVarianceFeatures(data, NearZeroVarThresh = NearZeroVarThresh)},
                 error = function(x) NULL)
-  if(!is.na(a)) {
+  if(any(class(a) %chin% c("data.table"))) {
     z <- z + 1
     collect[[z]] <- a
   }
-
+  
   # HighCardinalityFeatures Run----
   b <- tryCatch({HighCardinalityFeatures(data, CharUniqThresh = CharUniqThresh)},
                 error = function(x) NULL)
-  if(!is.na(b)) {
+  if(any(class(b) %chin% c("data.table"))) {
     z <- z + 1
     collect[[z]] <- b
   }
-
+  
   # HighMissingCountFeatures Run----
   c <- tryCatch({HighMissingCountFeatures(data, NA_Rate = NA_Rate)},
                 error = function(x) NULL)
-  if(!is.na(c)) {
+  if(any(class(c) %chin% c("data.table"))) {
     z <- z + 1
     collect[[z]] <- c
   }
-
+  
   # HighZeroCountFeatures Run----
   d <- tryCatch({HighZeroCountFeatures(data, Zero_Rate = Zero_Rate)},
                 error = function(x) NULL)
-  if(!is.na(d)) {
+  if(any(class(d) %chin% c("data.table"))) {
     z <- z + 1
     collect[[z]] <- d
   }
-
+  
   # HighSkewFeatures Run----
   e <- tryCatch({HighSkewFeatures(data, HighSkewThresh = HighSkewThresh)},
                 error = function(x) NULL)
-  if(!is.na(e)) {
+  if(any(class(e) %chin% c("data.table"))) {
     z <- z + 1
     collect[[z]] <- e
   }
-
+  
   # Combine Outputs
   if(length(collect) == 0) {
     return(NULL)
