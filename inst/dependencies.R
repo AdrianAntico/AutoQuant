0a2dd22f47d0d6552a69b4386b1a058e675981e1
Adrian <adrianantico@gmail.com>
2019-05-19 20:09:06 -0700

    Add files via upload

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -6,5 +6,15 @@ to_install <- c("catboost", "caTools", "data.table", "doParallel", "foreach", "f
     if (!requireNamespace(i)) {
       message(paste("     installing", i))
       install.packages(i)
+    } else if(i == "catboost") {
+      devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
+    } else if(i == "h2o") {
+      if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+      if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+      pkgs <- c("RCurl","jsonlite")
+      for (pkg in pkgs) {
+        if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+      }
+      install.packages("h2o", type="source", repos="https://h2o-release.s3.amazonaws.com/h2o/rel-yates/3/R")
     }
   }
