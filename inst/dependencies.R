199467ee7a8f996f58847f207be7015bb795723c
Adrian <adrianantico@gmail.com>
2019-05-19 19:51:57 -0700

    Update dependencies.R

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,8 +1,6 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("caTools", "data.table", "doParallel", "foreach", "forecast", "ggplot2", "h2o", "itertools", "lubridate", "monreg", "pROC", "RColorBrewer", "recommenderlab", "ROCR", "scatterplot3d", "stringr", "tm", "tsoutliers", "wordcloud", "zoo"
-                #"catboost" 
-                )
+to_install <- c("h2o","caTools", "data.table", "doParallel", "foreach", "forecast", "ggplot2", "itertools", "lubridate", "monreg", "pROC", "RColorBrewer", "recommenderlab", "ROCR", "scatterplot3d", "stringr", "tm", "tsoutliers", "wordcloud", "zoo","catboost")
   for (i in to_install) {
     message(paste("looking for ", i))
     if (!requireNamespace(i)) {
@@ -12,12 +10,15 @@ to_install <- c("caTools", "data.table", "doParallel", "foreach", "forecast", "g
                                  subdir = 'catboost/R-package')
       } else if (i == "h2o") {
         if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+        Sys.sleep(3)
         if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+        Sys.sleep(3)
 
         # Next, we download packages that H2O depends on.
         pkgs <- c("RCurl","jsonlite")
+        Sys.sleep(3)
         for (pkg in pkgs) {
-          if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+          if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg)}
         }
 
         # Now we download, install and initialize the H2O package for R.
@@ -27,3 +28,5 @@ to_install <- c("caTools", "data.table", "doParallel", "foreach", "forecast", "g
       }
     }
   }
+
+
