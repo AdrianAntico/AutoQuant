2e150975619923a1e1af1713df1b38b29634ed8a
Adrian <adrianantico@gmail.com>
2019-05-19 19:41:14 -0700

    Update dependencies.R

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,6 +1,8 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("catboost", "caTools", "data.table", "doParallel", "foreach", "forecast", "ggplot2", "h2o", "itertools", "lubridate", "monreg", "pROC", "RColorBrewer", "recommenderlab", "ROCR", "scatterplot3d", "stringr", "tm", "tsoutliers", "wordcloud", "zoo")
+to_install <- c("caTools", "data.table", "doParallel", "foreach", "forecast", "ggplot2", "h2o", "itertools", "lubridate", "monreg", "pROC", "RColorBrewer", "recommenderlab", "ROCR", "scatterplot3d", "stringr", "tm", "tsoutliers", "wordcloud", "zoo",
+                #"catboost" 
+                )
   for (i in to_install) {
     message(paste("looking for ", i))
     if (!requireNamespace(i)) {
