23380012b16ec9615b317f5de5690c0f067ffc66
Adrian <adrianantico@gmail.com>
2019-05-27 01:21:44 -0700

    Update RemixAutoML.R

--- R/RemixAutoML.R
+++ R/RemixAutoML.R
@@ -23158,7 +23158,7 @@ AutoCatBoostScoring <- function(TargetType = NULL,
     }
   }
 
-  # Regression Initialize Catboost Data Conversion----
+  # Initialize Catboost Data Conversion----
   if (!is.null(CatFeatures)) {
     ScoringPool <-
       catboost::catboost.load_pool(ScoringData, cat_features = CatFeatures)
@@ -23364,7 +23364,7 @@ AutoXGBoostScoring <- function(TargetType = NULL,
                                MissFactor = MDP_MissFactor,
                                MissNum = MDP_MissNum)
 
-  # Regression Initialize Catboost Data Conversion----
+  # Initialize XGBoost Data Conversion----
   ScoringMatrix <-
     xgboost::xgb.DMatrix(as.matrix(ScoringData))
 
@@ -23388,6 +23388,178 @@ AutoXGBoostScoring <- function(TargetType = NULL,
       all = FALSE
     )
     predict[, Predictions := OriginalLevels][, OriginalLevels := NULL]
+  }
+
+  # Merge features back on----
+  if(ReturnFeatures) {
+    predict <- cbind(predict,ScoringMerge)
+  }
+
+  # Return data----
+  return(predict)
+}
+
+#' AutoH2OMLScoring is an automated scoring function that compliments the AutoH2o model training functions.
+#'
+#' AutoH2OMLScoring is an automated scoring function that compliments the AutoH2oGBM__() and AutoH2oDRF__() models training functions. This function requires you to supply features for scoring. It will run ModelDataPrep()to prepare your features for H2O data conversion and scoring.
+#'
+#' @family Supervised Learning
+#' @param TargetType Set this value to "regression", "classification", or "multiclass" to score models built using AutoH2o__Regression(), AutoH2o__Classify() or AutoH2o__MultiClass().
+#' @param ScoringData This is your data.table of features for scoring. Can be a single row or batch.
+#' @param FeatureColumnNames Supply either column names or column numbers used in the AutoH2o__() function
+#' @param ModelType Set to either "mojo" or "standard" depending on which version you saved
+#' @param H2OShutdown Set to TRUE is you are scoring a "standard" model file and you aren't planning on continuing to score.
+#' @param MaxMem Set to you dedicated amount of memory. E.g. "28G"
+#' @param JavaOptions Change the default to your machines specification if needed. Default is '-Xmx1g -XX:ReservedCodeCacheSize=256m',
+#' @param ModelPath Supply your path file used in the AutoH2o__() function
+#' @param ModelID Supply the model ID used in the AutoH2o__() function
+#' @param ReturnFeatures Set to TRUE to return your features with the predicted values.
+#' @param MDP_Impute Set to TRUE if you did so for modeling and didn't do so before supplying ScoringData in this function
+#' @param MDP_CharToFactor Set to TRUE to turn your character columns to factors if you didn't do so to your ScoringData that you are supplying to this function
+#' @param MDP_RemoveDates Set to TRUE if you have date of timestamp columns in your ScoringData
+#' @param MDP_MissFactor If you set MDP_Impute to TRUE, supply the character values to replace missing values with
+#' @param MDP_MissNum If you set MDP_Impute to TRUE, supply a numeric value to replace missing values with
+#' @examples
+#' \donttest{
+#' Preds <- AutoH2OMLScoring(ScoringData = data,
+#'                           FeatureColNames = 2:12,
+#'                           ModelType = "mojo",
+#'                           H2OShutdown = TRUE,
+#'                           MaxMem = "28G",
+#'                           JavaOptions = '-Xmx1g -XX:ReservedCodeCacheSize=256m',
+#'                           ModelPath = NULL,
+#'                           ModelID = "ModelTest",
+#'                           ReturnFeatures = TRUE,
+#'                           MDP_Impute = TRUE,
+#'                           MDP_CharToFactor = TRUE,
+#'                           MDP_RemoveDates = TRUE,
+#'                           MDP_MissFactor = "0",
+#'                           MDP_MissNum = -1)
+#' }
+#' @return A data.table of predicted values with the option to return model features as well.
+#' @export
+AutoH2OMLScoring <- function(TargetType = NULL,
+                             ScoringData = NULL,
+                             FeatureColNames = NULL,
+                             ModelType = "mojo",
+                             H2OShutdown = TRUE,
+                             MaxMem = "28G",
+                             JavaOptions = '-Xmx1g -XX:ReservedCodeCacheSize=256m',
+                             ModelPath = NULL,
+                             ModelID = NULL,
+                             ReturnFeatures = TRUE,
+                             MDP_Impute = TRUE,
+                             MDP_CharToFactor = TRUE,
+                             MDP_RemoveDates = TRUE,
+                             MDP_MissFactor = "0",
+                             MDP_MissNum = -1) {
+
+  # Ensure packages are available
+  requireNamespace('data.table', quietly = TRUE)
+
+  # Check arguments----
+  if(!file.exists(ModelPath)) {
+    warning("Model does not exist in the location provided by ModelPath")
+  }
+  if(is.null(ScoringData)) {
+    warning("ScoringData cannot be NULL")
+  }
+  if(is.null(FeatureColNames)) {
+    warning("FeatureColumnNames cannot be NULL")
+  }
+  if(!data.table::is.data.table(ScoringData)) {
+    ScoringData <- data.table::as.data.table(ScoringData)
+  }
+  if(!is.logical(MDP_Impute)) {
+    warning("MDP_Impute (ModelDataPrep) should be TRUE or FALSE")
+  }
+  if(!is.logical(MDP_CharToFactor)) {
+    warning("MDP_CharToFactor (ModelDataPrep) should be TRUE or FALSE")
+  }
+  if(!is.logical(MDP_RemoveDates)) {
+    warning("MDP_RemoveDates (ModelDataPrep) should be TRUE or FALSE")
+  }
+  if(!is.character(MDP_MissFactor) & !is.factor(MDP_MissFactor)) {
+    warning("MDP_MissFactor should be a character or factor value")
+  }
+  if(!is.numeric(MDP_MissNum)) {
+    warning("MDP_MissNum should be a numeric or integer value")
+  }
+
+  # ScoringData Subset Columns Needed----
+  if (is.numeric(FeatureColNames) | is.integer(FeatureColNames)) {
+    keep1 <- names(ScoringData)[c(FeatureColNames)]
+    if (!is.null(IDcols)) {
+      keep <- c(IDcols, keep1)
+    } else {
+      keep <- c(keep1)
+    }
+    ScoringData <- ScoringData[, ..keep]
+    if(ReturnFeatures) {
+      ScoringMerge <- data.table::copy(ScoringData)
+      ScoringData <- ScoringData[, ..keep1]
+    }
+  } else {
+    keep1 <- c(FeatureColNames)
+    if (!is.null(IDcols)) {
+      keep <- c(IDcols, FeatureColNames)
+    } else {
+      keep <- c(FeatureColNames)
+    }
+    ScoringData <- ScoringData[, ..keep]
+    if(ReturnFeatures) {
+      ScoringMerge <- data.table::copy(ScoringData)
+      ScoringData <- ScoringData[, ..keep1]
+    }
+  }
+
+  # ModelDataPrep Check----
+  ScoringData <- ModelDataPrep(data = ScoringData,
+                               Impute = MDP_Impute,
+                               CharToFactor = MDP_CharToFactor,
+                               RemoveDates = MDP_RemoveDates,
+                               MissFactor = MDP_MissFactor,
+                               MissNum = MDP_MissNum)
+
+  # Initialize H2O Data Conversion----
+  if(tolower(ModelType) != "mojo") {
+    h2o::h2o.init(max_mem_size = MaxMem,
+                  enable_assertions = FALSE)
+    ScoreData    <- h2o::as.h2o(ScoringData)
+  } else {
+    ScoreData <- ScoringData
+  }
+
+  # Make Predictions----
+  if(tolower(ModelType) == "mojo") {
+    predict <- data.table::as.data.table(
+      h2o::h2o.mojo_predict_df(frame = ScoreData,
+                               mojo_zip_path = file.path(ModelPath,paste0(ModelID,".zip")),
+                               genmodel_jar_path = file.path(ModelPath,paste0(ModelID)),
+                               java_options = JavaOptions)
+    )
+
+  } else if(tolower(ModelType) == "standard") {
+    model <- h2o::h2o.loadModel(path = paste0(ModelPath,"/",ModelID))
+    predict <- data.table::as.data.table(
+      h2o::h2o.predict(object = model,
+                       newdata = ScoreData)
+    )
+  }
+
+  # Change column name----
+  data.table::setnames(predict, "predict", "Predictions")
+
+  # Merge features----
+  if(ReturnFeatures) {
+    ReturnPreds <- cbind(predict, ScoringData)
+  }
+
+  # Shut down H2O----
+  if(tolower(ModelType) != "mojo") {
+    if(H2OShutdown) {
+      h2o::h2o.shutdown(prompt = FALSE)
+    }
   }
 
   # Merge features back on----
