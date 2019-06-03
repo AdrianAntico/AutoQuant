70113ff557129f33b611361eea0e36f46e213bae
Adrian <adrianantico@gmail.com>
2019-06-03 13:18:01 -0700

    Add files via upload

---
+++ R/Imports.R
@@ -0,0 +1,6 @@
+#' @import data.table
+#' @import foreach
+#' @importFrom data.table data.table %chin% .I .N .SD := as.data.table fwrite is.data.table rbindlist set setcolorder setnames setorderv as.IDate as.ITime
+NULL
+
+.datatable.aware = TRUE
