2352fcb8c63d9c2cbfe88a20cf26c38a823bbf34
Adrian <adrianantico@gmail.com>
2019-06-30 15:53:26 -0700

    Update Imports.R

--- R/Imports.R
+++ R/Imports.R
@@ -2,7 +2,8 @@
 #' @import foreach
 #' @importFrom data.table data.table %chin% .I .N .SD := as.data.table fwrite is.data.table rbindlist set setcolorder setnames setorderv as.IDate as.ITime
 #' @importFrom lubridate %m+%
-#' @importFrom doParallel %dopar%
+#' @importFrom foreach %dopar%
+#' @import doParallel
 NULL
 
 .datatable.aware = TRUE
