88f5159ce86596ea9e0c260382dfe4b334c93931
Adrian <adrianantico@gmail.com>
2019-07-10 21:07:26 -0700

    Add files via upload

--- R/Imports.R
+++ R/Imports.R
@@ -3,6 +3,7 @@
 #' @importFrom data.table data.table %chin% .I .N .SD := as.data.table fwrite is.data.table rbindlist set setcolorder setnames setorderv as.IDate as.ITime
 #' @importFrom lubridate %m+%
 #' @importFrom foreach %dopar%
+#' @importFrom stats optimize pchisq
 #' @import doParallel
 NULL
 
