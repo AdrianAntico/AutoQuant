85b7301e052d0122fa5bdd56d3029e0c2c6258e4
Adrian <adrianantico@gmail.com>
2019-07-15 10:07:55 -0700

    Update AutoCatBoostScoring.R

--- R/AutoCatBoostScoring.R
+++ R/AutoCatBoostScoring.R
@@ -11,7 +11,7 @@
 #' @param ModelPath Supply your path file used in the AutoCatBoost__() function
 #' @param ModelID Supply the model ID used in the AutoCatBoost__() function
 #' @param ReturnFeatures Set to TRUE to return your features with the predicted values.
-#' @param MulitClassTargetLevels For use with AutoCatBoostMultiClass(). If you saved model objects then this scoring function will locate the target levels file. If you did not save model objects, you can supply the target levels returned from AutoCatBoostMultiClass().
+#' @param MultiClassTargetLevels For use with AutoCatBoostMultiClass(). If you saved model objects then this scoring function will locate the target levels file. If you did not save model objects, you can supply the target levels returned from AutoCatBoostMultiClass().
 #' @param TransformNumeric Set to TRUE if you have features that were transformed automatically from an Auto__Regression() model AND you haven't already transformed them.
 #' @param BackTransNumeric Set to TRUE to generate back-transformed predicted values. Also, if you return features, those will also be back-transformed.
 #' @param TargetColumnName Input your target column name used in training if you are utilizing the transformation service
@@ -33,7 +33,7 @@
 #'                              ModelPath = "home",
 #'                              ModelID = "ModelTest",
 #'                              ReturnFeatures = TRUE,
-#'                              MulitClassTargetLevels = NULL,
+#'                              MultiClassTargetLevels = NULL,
 #'                              TransformNumeric = FALSE,
 #'                              BackTransNumeric = FALSE,
 #'                              TargetColumnName = NULL,
@@ -56,7 +56,7 @@ AutoCatBoostScoring <- function(TargetType = NULL,
                                 ModelPath = NULL,
                                 ModelID = NULL,
                                 ReturnFeatures = TRUE,
-                                MulitClassTargetLevels = NULL,
+                                MultiClassTargetLevels = NULL,
                                 TransformNumeric = FALSE,
                                 BackTransNumeric = FALSE,
                                 TargetColumnName = NULL,
@@ -244,8 +244,8 @@ AutoCatBoostScoring <- function(TargetType = NULL,
   if (tolower(TargetType) != "multiclass") {
     data.table::setnames(predict, "V1", "Predictions")
   } else if (tolower(TargetType) == "multiclass") {
-    if(!is.null(MulitClassTargetLevels)) {
-      TargetLevels <- MulitClassTargetLevels
+    if(!is.null(MultiClassTargetLevels)) {
+      TargetLevels <- MultiClassTargetLevels
     } else {
       TargetLevels <-
         data.table::fread(paste0(ModelPath, "/", ModelID, "_TargetLevels.csv"))      
