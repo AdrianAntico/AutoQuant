32fec2269cf93c8a98f1cb9e815e6b469d7d18f4
Adrian <adrianantico@gmail.com>
2019-09-15 14:52:52 -0700

    New function added

--- R/AutoFreqSizeDistSimFunctions.R
+++ R/AutoFreqSizeDistSimFunctions.R
@@ -57,3 +57,102 @@ ID_SingleLevelGibbsSampler <- function(CountDataLevel,
       list(
         SimResults)))
 }
+
+#' ID_Forecast for forecasting intermittent demand data
+#' 
+#' ID_Forecast for forecasting intermittent demand data. It runs collapsed gibbs sampler simulations, using the count and size quantile regression models, for every period for all group levels in your data.
+#' 
+#' @family Automated Time Series
+#' @author Adrian Antico
+#' @param CountData This is the count data returned from AutoCatBoostFreqSizeScoring() or AutoH2oGBMFreqSizeScoring()
+#' @param SizeData This is the size data returned from AutoCatBoostFreqSizeScoring() or AutoH2oGBMFreqSizeScoring()
+#' @param GroupVar This is your grouping variable. E.g. sku as being the name of the column that contains all skus
+#' @param FC_Periods The max period of your forecast. E.g. if you want 52 weeks of forecasts, set to 52
+#' @param NumSims Set the number of collapsed gibbs simulations to run for each time unit forecast. E.g. if you want 52 weeks of forecasts, the simulations will run NumSims for each period up to 52.
+#' @param PredictionIntervals Set the prediction intervals you want returned. E.g. c(seq(0.05,0.95,0.05))
+#' @examples 
+#' \donttest{
+#' Results <- ID_Forecast(
+#'   CountData, 
+#'   SizeData, 
+#'   GroupVar = "sku", 
+#'   FC_Periods = 26, 
+#'   NumSims = 1000, 
+#'   PredictionIntervals = c(0.05,0.20,0.50,0.80,0.95))
+#' }
+#' @return Returns your entire set of groupvar forecasts in a single data.table
+#' @export
+ID_Forecast <- function(CountData, 
+                        SizeData, 
+                        GroupVar = NULL, 
+                        FC_Periods = 26, 
+                        NumSims = 1000, 
+                        PredictionIntervals = c(0.05,0.20,0.80,0.95)) {
+  
+  # Ensure data.tables----
+  if(!data.table::is.data.table(CountData)) {
+    CountData <- data.table::as.data.table(CountData)
+  }
+  if(!data.table::is.data.table(SizeData)) {
+    SizeData <- data.table::as.data.table(SizeData)
+  }
+  
+  # Forecast----
+  Counter = 0L
+  GroupVariable <- CountData[[eval(GroupVar)]]
+  for(Level in GroupVariable) {
+    
+    # Increment counter----
+    Counter <- Counter + 1L
+    
+    # Run ID_SIngleLevelGibbsSampler()----
+    SingleLevelData <- ID_SingleLevelGibbsSampler(
+      CountDataLevel = CountData[get(GroupVar) == eval(Level)],
+      SizeDataLevel = SizeData[get(GroupVar) == eval(Level)],
+      FC_Periods = FC_Periods, 
+      nSims = NumSims, 
+      CountList = seq(0.1,0.9,0.1),
+      SizeList = seq(0.1,0.9,0.1))
+    
+    # Replace all NaN with 0----
+    for(miss in seq_len(FC_Periods)) {
+      data.table::set(
+        SingleLevelData, 
+        i = which(is.na(SingleLevelData[[paste0("V",miss)]])), 
+        j = paste0("V",miss), 
+        value = 0)
+    }
+
+    # Create Final Data----
+    FinalData <- data.table::data.table(GroupVar = Level, Periods = 1:FC_Periods, Mean = 0)
+    for(PI in PredictionIntervals) {
+      data.table::set(FinalData, j = paste0("PI_",PI*100), value = 0)
+    }
+    
+    # Fill out data----
+    for(period in as.integer(seq_len(FC_Periods))) {
+      
+      # Fill out mean value----
+      data.table::set(
+        x = FinalData,
+        i = period,
+        j = "Mean",
+        value = mean(SingleLevelData[[paste0("V",period)]]))
+      
+      # Fill out percentiles----
+      for(PI in PredictionIntervals) {
+        data.table::set(FinalData, i = period, j = paste0("PI_",PI*100), value = quantile(x = SingleLevelData[[paste0("V",period)]],probs = PI))
+      }      
+    }
+    
+    # Combine data----
+    if(Counter == 1L) {
+      FinalDataReturn <- FinalData
+    } else {
+      FinalDataReturn <- data.table::rbindlist(
+        list(
+          FinalDataReturn, 
+          FinalData))
+    }
+  }
+}
