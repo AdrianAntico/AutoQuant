46a177c7ab9905e83c831d52f7c8e6996f450406
Adrian <adrianantico@gmail.com>
2020-02-06 10:52:41 -0700

    Update AutoCatBoostRegression.R

--- R/AutoCatBoostRegression.R
+++ R/AutoCatBoostRegression.R
@@ -1182,7 +1182,7 @@ AutoCatBoostRegression <- function(data,
   
   # VI_Plot_Function
   VI_Plot <- function(VI_Data, ColorHigh = "darkblue", ColorLow = "white") {
-    ggplot2::ggplot(VI_Data, ggplot2::aes(x = reorder(Variable, Importance), y = Importance, fill = Importance)) +
+    ggplot2::ggplot(VI_Data[1:min(10,.N)], ggplot2::aes(x = reorder(Variable, Importance), y = Importance, fill = Importance)) +
       ggplot2::geom_bar(stat = "identity") +
       ggplot2::scale_fill_gradient2(
         mid = ColorLow,
