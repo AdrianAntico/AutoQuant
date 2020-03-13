f0a08d8ee0532d88631e20de6ab636a311d843a8
Adrian <adrianantico@gmail.com>
2020-03-13 12:14:33 -0700

    adsf

--- R/AutoH2oDRFClassifier.R
+++ R/AutoH2oDRFClassifier.R
@@ -602,7 +602,7 @@ AutoH2oDRFClassifier <- function(data,
   
   # VI_Plot_Function
   VI_Plot <- function(VI_Data, ColorHigh = "darkblue", ColorLow = "white") {
-    ggplot2::ggplot(VI_Data[1:min(10,.N)], ggplot2::aes(x = reorder(Variable, Importance), y = Importance, fill = Importance)) +
+    ggplot2::ggplot(VI_Data[1:min(10,.N)], ggplot2::aes(x = reorder(Variable, Percentage), y = Percentage, fill = Percentage)) +
       ggplot2::geom_bar(stat = "identity") +
       ggplot2::scale_fill_gradient2(mid = ColorLow,high = ColorHigh) +
       ChartTheme(Size = 12,AngleX = 0,LegendPosition = "right") +
