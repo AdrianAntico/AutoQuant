5b638786c8311e715c9428a87559ba49dbedf61c
Adrian <adrianantico@gmail.com>
2020-03-24 10:57:34 -0700

    Update function

--- R/IntermittentDemandDataGeneratorFunctions.R
+++ R/IntermittentDemandDataGeneratorFunctions.R
@@ -11,6 +11,7 @@
 #' @param FilePath Set to your file of choice for where you want the data sets saved
 #' @param TargetVariableName The name of your target variable that represents demand
 #' @param DateVariableName  The date variable of the demand instances
+#' @param GDL_Targets The variable names to run through AutoLagRollStats()
 #' @param GroupingVariables These variables (or sinlge variable) is the combination of categorical variables that uniquely defines the level of granularity of each individual level to forecast. E.g. "sku" or c("Store","Department"). Sku is typically unique for all sku's. Store and Department in combination defines all unique departments as the department may be repeated across the stores.
 #' @param MinTimeWindow The number of time periods you would like to omit for training. Default is 1 so that at a minimum, there is at least one period of values to forecast. You can set it up to a larger value if you do not want more possible target windows for the lower target window values.
 #' @param MinTxnRecords I typically set this to 2 so that there is at least one other instance of demand so that the forecasted values are not complete nonsense.
@@ -32,6 +33,7 @@
 #'                                             FilePath = NULL,
 #'                                             TargetVariableName = "qty",
 #'                                             DateVariableName = "date",
+#'                                             GDL_Targets = NULL,
 #'                                             GroupingVariables = "sku",
 #'                                             MinTimeWindow = 1,
 #'                                             MinTxnRecords = 2,
@@ -65,6 +67,7 @@ IntermittentDemandDataGenerator <- function(data,
                                             FilePath = NULL,
                                             TargetVariableName = "qty",
                                             DateVariableName = "date",
+                                            GDL_Targets = NULL,
                                             TimeUnit = "raw",
                                             TimeGroups = c("raw","day","week"),
                                             GroupingVariables = "sku",
@@ -202,7 +205,7 @@ IntermittentDemandDataGenerator <- function(data,
   
   # Print Steps----
   if(PrintSteps) {
-    print("Running AutoLagRollStats()") 
+    print("Running DT_GDL_Feature_Engineering()") 
   }
   
   # Add in the time varying features----
@@ -255,7 +258,7 @@ IntermittentDemandDataGenerator <- function(data,
   }
   
   # Run Final Build----
-  packages <- c("RemixAutoML","data.table","forecast","lubridate")
+  packages <- c("RemixAutoAI","data.table","forecast","lubridate")
   cores <- parallel::detectCores()
   
   # Create File Splitter Column Indicator----
@@ -334,7 +337,6 @@ IntermittentDemandDataGenerator <- function(data,
 #'
 #' @param data This is your transactional level data
 #' @param RestrictDateRange = TRUE
-#' @param TargetVariableName Bla
 #' @param DateVariableName Bla
 #' @param GroupingVariables Bla
 #' @param MinTimeWindow The number of time periods you would like to omit for training. Default is 1 so that at a minimum, there is at least one period of values to forecast. You can set it up to a larger value if you do not want more possible target windows for the lower target window values.
@@ -347,7 +349,6 @@ IntermittentDemandDataGenerator <- function(data,
 #' MetaData <- ID_MetadataGenerator(
 #'   data = data,
 #'   RestrictDateRange = TRUE,
-#'   TargetVariableName = TargetVariableName,
 #'   DateVariableName = DateVariableName,
 #'   GroupingVariables = GroupingVariables,
 #'   MinTimeWindow = MinTimeWindow,
@@ -359,7 +360,6 @@ IntermittentDemandDataGenerator <- function(data,
 #' @return Returns a data.table with summary information for the IntermittentDemandBootStrapper() function.
 ID_MetadataGenerator <- function(data,
                                  RestrictDateRange = TRUE,
-                                 TargetVariableName = NULL,
                                  DateVariableName = NULL,
                                  GroupingVariables = NULL,
                                  MinTimeWindow = 1,
@@ -422,6 +422,7 @@ ID_MetadataGenerator <- function(data,
 #' ID_TrainingDataGenerator for subsetting data for the IntermittentDemandBootStrapper() function.
 #' 
 #' @param data Source data
+#' @param Type "timetoevent1", "eventinwindow1"
 #' @param TargetVariableName Name of the variables to run feature engineering on. List the actual target variable name first.
 #' @param Level The individual level of your group variable
 #' @param GroupingVariables Your grouping variables
@@ -432,12 +433,13 @@ ID_MetadataGenerator <- function(data,
 #' @noRd
 #' @return Returns two data sets for the IntermittentDemandBootStrapper() function based on a single level from the grouping variables.
 ID_TrainingDataGenerator <- function(data,
+                                     Type = "timetoevent1",
                                      TargetVariableName = NULL,
                                      Level = NULL,
                                      DateVariableName = NULL,
                                      RandomStartDate = NULL,
                                      TimeUnit = NULL,
-                                     TargetWindow) {
+                                     TargetWindow = NULL) {
   
   # historical data <--> point in time <--> target window----
   histDemandRaw <- data[get(DateVariableName) < eval(RandomStartDate)]
@@ -445,6 +447,8 @@ ID_TrainingDataGenerator <- function(data,
   # Data within target window----
   counter <- 0L
   for (tar in TargetWindow) {
+    
+    # Target variable data
     if(lubridate::is.POSIXct(data[[eval(DateVariableName)]])) {
       targetDemand  <- data[
         get(DateVariableName) >= eval(RandomStartDate) &
@@ -501,12 +505,84 @@ ID_TrainingDataGenerator <- function(data,
       SizeData = SizeFinal))
 }
 
+#' ID_TrainingDataGenerator2 for subsetting data
+#' 
+#' ID_TrainingDataGenerator2 for subsetting data for the IntermittentDemandBootStrapper() function.
+#' 
+#' @param data Source data
+#' @param TargetVariableName vector of variable names
+#' @param Level The individual level of your group variable
+#' @param GroupingVariables Your grouping variables
+#' @param DateVariableName Name of your date variable
+#' @param RandomStartDate The date to partition the data
+#' @param TimeUnit This is the TimeUnit you selected for aggregation
+#' @param TargetWindow The length of the target window sampled
+#' @noRd
+#' @return Returns two data sets for the IntermittentDemandBootStrapper() function based on a single level from the grouping variables.
+ID_TrainingDataGenerator2 <- function(data,
+                                      TargetVariableName = NULL,
+                                      Level = NULL,
+                                      DateVariableName = NULL,
+                                      RandomStartDate = NULL,
+                                      TimeUnit = NULL,
+                                      TargetWindow = NULL) {
+  
+  # historical data <--> point in time <--> target window----
+  histDemandRaw <- data[get(DateVariableName) < eval(RandomStartDate)]
+  
+  # Data within target window----
+  counter <- 0L
+  for (tar in TargetWindow) {
+    
+    # Classification target variable data----
+    if(lubridate::is.POSIXct(data[[eval(DateVariableName)]])) {
+      binarytarget  <- min(data[get(DateVariableName) >= eval(RandomStartDate) & get(DateVariableName) - 86400 * eval(tar) <= eval(RandomStartDate), 
+                                get(TargetVariableName[1L])], na.rm = TRUE)
+    } else {
+      binarytarget  <- min(data[get(DateVariableName) >= eval(RandomStartDate) & get(DateVariableName) - eval(tar) <= eval(RandomStartDate), 
+                                get(TargetVariableName[1L])], na.rm = TRUE)
+    }
+    
+    # Time to event target variable data----
+    timetoevent <- data[1L, get(TargetVariableName[2L])]
+    outcome <- data[1L, get(TargetVariableName[3L])]
+    
+    # Add in the time since last demand instance from RandomStartDate----
+    histDemandRaw <- histDemandRaw[order(-get(DateVariableName))][
+      , TimeSinceLastDemand := as.numeric(difftime(RandomStartDate,get(DateVariableName), units = TimeUnit))]
+    
+    # Remove meta data for feature creation set----
+    features <- histDemandRaw[order(-get(DateVariableName))][
+      , paste0(eval(DateVariableName)) := NULL][1,]
+    data.table::set(features, j = "FC_Window", value = tar)
+    
+    # Merge Features and Targets----
+    temp <- cbind(binarytarget, timetoevent, outcome, features)
+    data.table::setnames(temp, names(temp)[1L], eval(TargetVariableName[1L]))
+    data.table::setnames(temp, names(temp)[2L], eval(TargetVariableName[2L]))
+    data.table::setnames(temp, names(temp)[3L], eval(TargetVariableName[3L]))
+    data.table::set(temp, data.table::fifelse(temp[[eval(TargetVariableName)]] == 0, 1, 0))
+    
+    # Combine data sets----
+    counter <- counter + 1L
+    if(counter == 1L) {
+      Final <- temp
+    } else {
+      Final <- data.table::rbindlist(list(Final,temp), fill = TRUE)
+    }
+  }
+  
+  # Output data file----
+  return(data = Final)
+}
+
 #' ID_BuildTrainDataSets for assembling data
 #' 
 #' ID_BuildTrainDataSets for assembling data for the IntermittentDemandBootStrapper() function.
 #' 
 #' @param MetaData This is the metadata returned from the ID_MetadataGenerator() function
 #' @param data This is your transactional data
+#' @param Case Indicate which data constructor method to use
 #' @param TargetVariableName Your target variable names
 #' @param DateVariableName Your date variable names
 #' @param GroupingVariables Your grouping variables
@@ -519,6 +595,7 @@ ID_TrainingDataGenerator <- function(data,
 #' @return Returns the count modeling data and the size modeling data
 ID_BuildTrainDataSets <- function(MetaData,
                                   data,
+                                  Case = 2L,
                                   TargetVariableName = NULL,
                                   DateVariableName = NULL,
                                   GroupingVariables = NULL,
@@ -600,13 +677,23 @@ ID_BuildTrainDataSets <- function(MetaData,
                              replace = TRUE)
       
       # Create samples----
-      SampleData <- ID_TrainingDataGenerator(
-        data = level_data,
-        TargetVariableName = TargetVariableName,
-        DateVariableName = DateVariableName,
-        RandomStartDate = RandomStartDate,
-        TimeUnit = TimeUnit,
-        TargetWindow = TargetWindow)
+      if(Case == 1L) {
+        SampleData <- ID_TrainingDataGenerator(
+          data = level_data,
+          TargetVariableName = TargetVariableName,
+          DateVariableName = DateVariableName,
+          RandomStartDate = RandomStartDate,
+          TimeUnit = TimeUnit,
+          TargetWindow = TargetWindow)
+      } else if(Case == 2L) {
+        SampleData <- ID_TrainingDataGenerator2(
+          data = level_data,
+          TargetVariableName = TargetVariableName,
+          DateVariableName = DateVariableName,
+          RandomStartDate = RandomStartDate,
+          TimeUnit = TimeUnit,
+          TargetWindow = TargetWindow)
+      }
       
       # Build data sets----
       if(i == 1L) {
