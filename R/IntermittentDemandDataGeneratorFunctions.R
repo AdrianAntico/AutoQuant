22365ee6d39fa4f70c7043f6fd3f523e2276479d
Adrian <adrianantico@gmail.com>
2020-03-24 14:48:32 -0700

    Update functions

--- R/IntermittentDemandDataGeneratorFunctions.R
+++ R/IntermittentDemandDataGeneratorFunctions.R
@@ -280,12 +280,10 @@ IntermittentDemandDataGenerator <- function(data,
   
   # Print Steps----
   if(PrintSteps) {
-    print(names(MetaData))
     print("ParallelBuilding()") 
   }
   
   # Parallelize Build----
-  print(MetaData[SelectRows %in% c(1,2,3,4,5,6)])
   cl <- parallel::makePSOCKcluster(cores)
   doParallel::registerDoParallel(cl)
   Results <- foreach::foreach(
