b0deba0610bf62f216ef74778debad30b19cfea4
Adrian <adrianantico@gmail.com>
2020-05-14 11:43:47 -0700

    Code cleanup

--- R/CreateHolidayVariables.R
+++ R/CreateHolidayVariables.R
@@ -13,25 +13,23 @@
 #' @examples
 #' \donttest{
 #' data <- data.table::data.table(Date = '2018-01-01 00:00:00')
-#' data <- CreateHolidayVariables(data,
-#'                                DateCols = "DateTime",
-#'                                HolidayGroups = c("USPublicHolidays","EasterGroup",
-#'                                                  "ChristmasGroup","OtherEcclesticalFeasts"),
-#'                                Holidays = NULL,
-#'                                GroupingVars = NULL)
+#' data <- CreateHolidayVariables(
+#'    data,
+#'    DateCols = "DateTime",
+#'    HolidayGroups = c("USPublicHolidays","EasterGroup","ChristmasGroup","OtherEcclesticalFeasts"),
+#'    Holidays = NULL,
+#'    GroupingVars = NULL)
 #' }
 #' @return Returns your data.table with the added holiday indicator variable
 #' @export
 CreateHolidayVariables <- function(data,
-                                   DateCols = "DateTime",
-                                   HolidayGroups = c("USPublicHolidays","EasterGroup",
-                                                     "ChristmasGroup",
-                                                     "OtherEcclesticalFeasts"),
+                                   DateCols = NULL,
+                                   HolidayGroups = c("USPublicHolidays","EasterGroup","ChristmasGroup","OtherEcclesticalFeasts"),
                                    Holidays = NULL,
                                    GroupingVars = NULL) {
   
   # Turn on full speed ahead----
-  data.table::setDTthreads(percent = 100)
+  data.table::setDTthreads(threads = max(1L, parallel::detectCores()-2L))
   
   # If GroupVars are numeric, convert them to character
   for(zz in seq_along(GroupingVars)) {
@@ -50,15 +48,11 @@ CreateHolidayVariables <- function(data,
   }
   
   # Convert to data.table----
-  if (!data.table::is.data.table(data)) {
-    data <- data.table::as.data.table(data)
-  }
+  if(!data.table::is.data.table(data)) data <- data.table::as.data.table(data)
   
   # Sort by group and date----
   if(!is.null(GroupingVars)) {
-    if(!any(class(data[[eval(DateCols)]]) %chin% c("POSIXct","POSIXt","Date"))) {
-      data[, eval(DateCols) := as.POSIXct(data[[eval(DateCols)]])]
-    }
+    if(!any(class(data[[eval(DateCols)]]) %chin% c("POSIXct","POSIXt","Date"))) data[, eval(DateCols) := as.POSIXct(data[[eval(DateCols)]])]
     data <- data[order(get(GroupingVars),get(DateCols))]
   }
   
@@ -66,7 +60,7 @@ CreateHolidayVariables <- function(data,
   if(!is.null(HolidayGroups)) {
     for(counter in seq_len(length(HolidayGroups))) {
       if(tolower(HolidayGroups[counter]) == "eastergroup") {
-        if(counter == 1) {
+        if(counter == 1L) {
           Holidays <- c("Septuagesima", "Quinquagesima","AshWednesday", 
                         "PalmSunday","GoodFriday","EasterSunday",
                         "Easter","EasterMonday","RogationSunday",
@@ -81,7 +75,7 @@ CreateHolidayVariables <- function(data,
         }
       }
       if(tolower(HolidayGroups[counter]) == "christmasgroup") {
-        if(counter == 1) {
+        if(counter == 1L) {
           Holidays <- c("ChristTheKing","Advent1st","Advent1st",
                         "Advent3rd","Advent4th","ChristmasEve",
                         "ChristmasDay","BoxingDay","NewYearsDay")
@@ -92,7 +86,7 @@ CreateHolidayVariables <- function(data,
         }
       }
       if(tolower(HolidayGroups[counter]) == "otherecclesticalfeasts") {
-        if(counter == 1) {
+        if(counter == 1L) {
           Holidays <- c("SolemnityOfMary","Epiphany","PresentationOfLord",
                         "Annunciation","TransfigurationOfLord","AssumptionOfMary",
                         "AssumptionOfMary","BirthOfVirginMary","CelebrationOfHolyCross",
@@ -105,7 +99,7 @@ CreateHolidayVariables <- function(data,
         }
       }
       if(tolower(HolidayGroups[counter]) == "uspublicholidays") {
-        if(counter == 1) {
+        if(counter == 1L) {
           Holidays <- c("USNewYearsDay","USInaugurationDay","USGoodFriday",
                         "USMLKingsBirthday","USLincolnsBirthday","USWashingtonsBirthday",
                         "USMemorialDay","USIndependenceDay","USLaborDay",
@@ -124,16 +118,10 @@ CreateHolidayVariables <- function(data,
   
   # Run holiday function to get unique dates----
   library(timeDate)
-  Holidays <- unique(as.Date(timeDate::holiday(
-    year = unique(lubridate::year(data[[eval(DateCols)]])), 
-    Holiday = Holidays)))
+  Holidays <- unique(as.Date(timeDate::holiday(year = unique(lubridate::year(data[[eval(DateCols)]])), Holiday = Holidays)))
   
   # Turn DateCols into character names if not already----
-  for (i in DateCols) {
-    if (!is.character(DateCols[i])) {
-      DateCols[i] <- names(data)[DateCols[i]]
-    }
-  }
+  for(i in DateCols) if(!is.character(DateCols[i])) DateCols[i] <- names(data)[DateCols[i]]
   
   # Allocate data.table cols----
   data.table::alloc.col(DT = data, ncol(data) + 1L)
@@ -141,15 +129,15 @@ CreateHolidayVariables <- function(data,
   # Create Temp Date Columns----
   MinDate <- data[, min(get(DateCols[1]))]
   if(!is.null(GroupingVars)) {
-    for (i in seq_len(length(DateCols))) {
-      data.table::setorderv(x = data, cols = c(eval(GroupingVars), eval(DateCols[i])), order = 1, na.last = TRUE)
+    for(i in seq_len(length(DateCols))) {
+      data.table::setorderv(x = data, cols = c(eval(GroupingVars), eval(DateCols[i])), order = 1L, na.last = TRUE)
       data[, paste0("Lag1_", eval(DateCols[i])) := data.table::shift(
         x = get(DateCols[i]), n = 1L, fill = MinDate, type = "lag"), 
         by = c(eval(GroupingVars))]
     }  
   } else {
-    for (i in seq_len(length(DateCols))) {
-      data.table::setorderv(x = data, cols = eval(DateCols[i]), order = 1, na.last = TRUE)
+    for(i in seq_len(length(DateCols))) {
+      data.table::setorderv(x = data, cols = eval(DateCols[i]), order = 1L, na.last = TRUE)
       data.table::set(data, j = paste0("Lag1_", eval(DateCols[i])),
                       value = data.table::shift(x = data[[eval(DateCols[i])]], n = 1L, fill = MinDate, type = "lag"))
     }
@@ -160,24 +148,17 @@ CreateHolidayVariables <- function(data,
   data[, eval(paste0("Lag1_",DateCols[i])) := get(DateCols[i]) - x]
   
   # Sort by group and date----
-  if(!is.null(GroupingVars)) {
-    data <- data[order(get(GroupingVars),get(DateCols))]
-  }
+  if(!is.null(GroupingVars)) data <- data[order(get(GroupingVars),get(DateCols))]
   
   # Build Features----
-  for (i in seq_len(length(DateCols))) {
+  for(i in seq_len(length(DateCols))) {
     EndDateVector <- data[[eval(DateCols[i])]]
     StartDateVector <- data[[paste0("Lag1_", eval(DateCols[i]))]]
     data.table::set(data, i = which(data[[eval(DateCols[i])]] == MinDate), j = eval(paste0("Lag1_",DateCols[i])), value = MinDate - x)
-    for(j in as.integer(seq_len(data[,.N]))) {
-      data.table::set(x = data, i = j, j = "HolidayCounts",
-                      value = HolidayCountsInRange(Start = StartDateVector[j],End = EndDateVector[j],Values = Holidays))
-    }
-    
-    # Remove Lag1date----
+    for(j in as.integer(seq_len(data[,.N]))) data.table::set(x = data, i = j, j = "HolidayCounts", value = HolidayCountsInRange(Start = StartDateVector[j],End = EndDateVector[j],Values = Holidays))
     data.table::set(data, j = eval(paste0("Lag1_",DateCols[i])), value = NULL)
   }
   
-  # Return data
+  # Return data----
   return(data)
 }
