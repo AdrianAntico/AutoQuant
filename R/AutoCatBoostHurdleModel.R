f643d402874cfd4d507d9932cf3be93144075541
Adrian <adrianantico@gmail.com>
2020-05-22 13:03:19 -0700

    Update function

--- R/AutoCatBoostHurdleModel.R
+++ R/AutoCatBoostHurdleModel.R
@@ -581,7 +581,6 @@ AutoCatBoostHurdleModel <- function(data = NULL,
   } else {
     if(length(IDcols) != 0L) {
       data.table::setcolorder(TestData, c(1L:2L, (3L + length(IDcols)):((3L + length(IDcols)) + 1L), 3L:(2L + length(IDcols)), (((3L + length(IDcols)) + 2L):ncol(TestData))))
-      data.table::setcolorder(TestData, c(5L:ncol(TestData), 1L:4L))
     }
   }
 
