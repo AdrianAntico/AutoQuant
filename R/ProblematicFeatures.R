edc4903d799d4b51ed0d89d0bacfaa6ac711e50e
Adrian <adrianantico@gmail.com>
2020-05-27 09:49:41 -0700

    Code cleanup

--- R/ProblematicFeatures.R
+++ R/ProblematicFeatures.R
@@ -36,51 +36,30 @@ ProblematicFeatures <- function(data,
                                 HighSkewThresh = 10) {
   
   # Turn on full speed ahead----
-  data.table::setDTthreads(percent = 100)
+  data.table::setDTthreads(threads = max(1L, parallel::detectCores() - 2L))
   
   # Convert to data.table----
-  if (!data.table::is.data.table(data))
-    data <- data.table::as.data.table(data)
+  if(!data.table::is.data.table(data)) data.table::setDT(data)
   
   # Subset columns of interest----
-  keep <- names(data)[ColumnNumbers]
-  data <- data[, ..keep]
+  data <- data[, .SD, .SDcols = names(data)[ColumnNumbers]]
   
   # Define Functions for Calculations----
   LowVarianceFeatures <- function(data, NearZeroVarThresh = 0.05) {
-    # Skip Option----
-    if (is.null(NearZeroVarThresh))
-      return(NULL)
-    
-    # Ensure argument is valid----
-    if (NearZeroVarThresh > 1)
-      warning("NearZeroVarThresh should be between zero and one")
-    
-    # Get Row Count----
+    if(is.null(NearZeroVarThresh)) return(NULL)
+    if(NearZeroVarThresh > 1) return("NearZeroVarThresh should be between zero and one")
     xx <- data[, .N]
-    
-    # Begin process----
     NumNearZeroVariance <- list()
-    for (i in seq_len(ncol(data))) {
-      if (is.numeric(data[[i]]) &
-          length(unique(data[[i]])) / xx < NearZeroVarThresh) {
-        NumNearZeroVariance[names(data)[i]] <-
-          round(length(unique(data[[i]])) / xx, 4)
+    for(i in seq_len(ncol(data))) {
+      if(is.numeric(data[[i]]) & length(unique(data[[i]])) / xx < NearZeroVarThresh) {
+        NumNearZeroVariance[names(data)[i]] <- round(length(unique(data[[i]])) / xx, 4L)
       }
     }
-    
-    if (length(NumNearZeroVariance) > 0) {
-      a <-
-        tryCatch({
-          data.table::as.data.table(data.table::melt(NumNearZeroVariance))
-        },
-        error = function(x)
-          NULL)
-      if (dim(a)[1] != 0) {
-        data.table::setnames(a,
-                             c("L1", "value"),
-                             c("ColName", "LowVarianceFeatures"))
-        data.table::setcolorder(a, c(2, 1))
+    if(length(NumNearZeroVariance) > 0L) {
+      a <- tryCatch({data.table::as.data.table(data.table::melt(NumNearZeroVariance))}, error = function(x) NULL)
+      if(dim(a)[1L] != 0L) {
+        data.table::setnames(a, c("L1", "value"), c("ColName", "LowVarianceFeatures"))
+        data.table::setcolorder(a, c(2L, 1L))
         return(a)
       } else {
         return(NULL)
@@ -90,39 +69,20 @@ ProblematicFeatures <- function(data,
     }
   }
   HighCardinalityFeatures <- function(data, CharUniqThresh = 0.50) {
-    # Skip Option----
-    if (is.null(CharUniqThresh))
-      return(NULL)
-    
-    # Ensure argument is valid----
-    if (CharUniqThresh > 1)
-      warning("CharUniqThresh should be between zero and one")
-    
-    # Get Row Count----
+    if(is.null(CharUniqThresh)) return(NULL)
+    if(CharUniqThresh > 1L) return("CharUniqThresh should be between zero and one")
     xx <- data[, .N]
-    
-    # Begin process----
     CharUniqueTooHigh <- list()
-    for (i in seq_len(ncol(data))) {
-      if ((is.character(data[[i]]) |
-           is.factor(data[[i]])) &
-          length(unique(data[[i]])) / xx > CharUniqThresh) {
-        CharUniqueTooHigh[names(data)[i]] <-
-          round(length(unique(data[[i]])) / xx, 4)
+    for(i in seq_len(ncol(data))) {
+      if((is.character(data[[i]]) | is.factor(data[[i]])) & length(unique(data[[i]])) / xx > CharUniqThresh) {
+        CharUniqueTooHigh[names(data)[i]] <- round(length(unique(data[[i]])) / xx, 4L)
       }
     }
-    if (length(CharUniqueTooHigh) > 0) {
-      a <-
-        tryCatch({
-          data.table::as.data.table(data.table::melt(CharUniqueTooHigh))
-        },
-        error = function(x)
-          NULL)
-      if (dim(a)[1] != 0) {
-        data.table::setnames(a,
-                             c("L1", "value"),
-                             c("ColName", "HighCardinalityFeatures"))
-        data.table::setcolorder(a, c(2, 1))
+    if(length(CharUniqueTooHigh) > 0L) {
+      a <- tryCatch({data.table::as.data.table(data.table::melt(CharUniqueTooHigh))}, error = function(x) NULL)
+      if(dim(a)[1L] != 0L) {
+        data.table::setnames(a, c("L1", "value"), c("ColName", "HighCardinalityFeatures"))
+        data.table::setcolorder(a, c(2L, 1L))
         return(a)
       } else {
         return(NULL)
@@ -132,36 +92,20 @@ ProblematicFeatures <- function(data,
     }
   }
   HighMissingCountFeatures <- function(data, NA_Rate = 0.20) {
-    # Skip Option----
-    if (is.null(NA_Rate))
-      return(NULL)
-    
-    # Ensure argument is valid----
-    if (NA_Rate > 1)
-      warning("HighSkewThresh should be between zero and one")
-    
-    # Get Row Count----
+    if(is.null(NA_Rate)) return(NULL)
+    if(NA_Rate > 1L) return("HighSkewThresh should be between zero and one")
     xx <- data[, .N]
-    
-    # Begin process----
     LargeNAs <- list()
-    for (i in seq_len(ncol(data))) {
-      if (sum(is.na(data[[i]]) / xx) > NA_Rate) {
-        LargeNAs[names(data)[i]] <- round(sum(is.na(data[[i]])) / xx, 4)
+    for(i in seq_len(ncol(data))) {
+      if(sum(is.na(data[[i]]) / xx) > NA_Rate) {
+        LargeNAs[names(data)[i]] <- round(sum(is.na(data[[i]])) / xx, 4L)
       }
     }
-    if (length(LargeNAs) > 0) {
-      a <-
-        tryCatch({
-          data.table::as.data.table(data.table::melt(LargeNAs))
-        },
-        error = function(x)
-          NULL)
-      if (dim(a)[1] != 0) {
-        data.table::setnames(a,
-                             c("L1", "value"),
-                             c("ColName", "HighMissingCountFeatures"))
-        data.table::setcolorder(a, c(2, 1))
+    if(length(LargeNAs) > 0L) {
+      a <- tryCatch({data.table::as.data.table(data.table::melt(LargeNAs))}, error = function(x) NULL)
+      if(dim(a)[1L] != 0L) {
+        data.table::setnames(a, c("L1", "value"), c("ColName", "HighMissingCountFeatures"))
+        data.table::setcolorder(a, c(2L, 1L))
         return(a)
       } else {
         return(NULL)
@@ -171,34 +115,19 @@ ProblematicFeatures <- function(data,
     }
   }
   HighZeroCountFeatures <- function(data, Zero_Rate = 0.20) {
-    # Skip Option----
-    if (is.null(Zero_Rate))
-      return(NULL)
-    
-    # Get Row Count----
+    if(is.null(Zero_Rate)) return(NULL)
     xx <- data[, .N]
-    
-    # Begin process----
     LargeZeros <- list()
-    for (i in seq_len(ncol(data))) {
-      if (is.numeric(data[[i]]) &
-          data[get(names(data)[i]) == 0, .N] / xx > Zero_Rate) {
-        LargeZeros[names(data)[i]] <-
-          round(data[get(names(data)[i]) == 0, .N] / xx, 4)
+    for(i in seq_len(ncol(data))) {
+      if(is.numeric(data[[i]]) & data[get(names(data)[i]) == 0, .N] / xx > Zero_Rate) {
+        LargeZeros[names(data)[i]] <- round(data[get(names(data)[i]) == 0, .N] / xx, 4L)
       }
     }
-    if (length(LargeZeros) > 0) {
-      a <-
-        tryCatch({
-          data.table::as.data.table(data.table::melt(LargeZeros))
-        },
-        error = function(x)
-          NULL)
-      if (dim(a)[1] != 0) {
-        data.table::setnames(a,
-                             c("L1", "value"),
-                             c("ColName", "HighZeroCountFeatures"))
-        data.table::setcolorder(a, c(2, 1))
+    if(length(LargeZeros) > 0L) {
+      a <- tryCatch({data.table::as.data.table(data.table::melt(LargeZeros))}, error = function(x) NULL)
+      if(dim(a)[1L] != 0L) {
+        data.table::setnames(a, c("L1", "value"), c("ColName", "HighZeroCountFeatures"))
+        data.table::setcolorder(a, c(2L, 1L))
         return(a)
       } else {
         return(NULL)
@@ -208,47 +137,25 @@ ProblematicFeatures <- function(data,
     }
   }
   HighSkewFeatures <- function(data, HighSkewThresh = 10) {
-    # Skip Option----
-    if (is.null(HighSkewThresh))
-      return(NULL)
-    
-    # Ensure argument is valid----
-    if (!is.numeric(HighSkewThresh) & !is.integer(HighSkewThresh)) {
-      warning("HighSkewThresh should a numeric value")
-    }
-    
-    # Get Row Count----
+    if(is.null(HighSkewThresh)) return(NULL)
+    if(!is.numeric(HighSkewThresh) & !is.integer(HighSkewThresh)) return("HighSkewThresh should a numeric value")
     xx <- data[, .N]
-    
-    # Begin process----
     HighSkew <- list()
-    for (i in seq_len(ncol(data))) {
-      if (is.numeric(data[[i]]) | is.integer(data[[i]])) {
-        x <- sort(x = data[[i]],
-                  na.last = TRUE,
-                  decreasing = TRUE)
-        if (!(max(data[[i]], na.rm = TRUE) == 0 &
-              min(data[[i]], na.rm = TRUE) == 0)) {
-          if (sum(x[1:(length(x) * (1 - 0.95))], na.rm = TRUE) /
-              sum(x[(length(x) * (1 - 0.95)):xx], na.rm = TRUE) > HighSkewThresh) {
-            HighSkew[names(data)[i]] <-
-              round(sum(x[1:length(x) * 0.05], na.rm = TRUE) /
-                      sum(x[(floor(length(x) *
-                                     (0.95)) + 1):length(x)], na.rm = TRUE), 4)
+    for(i in seq_len(ncol(data))) {
+      if(is.numeric(data[[i]]) | is.integer(data[[i]])) {
+        x <- sort(x = data[[i]], na.last = TRUE, decreasing = TRUE)
+        if(!(max(data[[i]], na.rm = TRUE) == 0 & min(data[[i]], na.rm = TRUE) == 0)) {
+          if(sum(x[1L:(length(x) * (1 - 0.95))], na.rm = TRUE) / sum(x[(length(x) * (1 - 0.95)):xx], na.rm = TRUE) > HighSkewThresh) {
+            HighSkew[names(data)[i]] <- round(sum(x[1:length(x) * 0.05], na.rm = TRUE) / sum(x[(floor(length(x) * (0.95)) + 1):length(x)], na.rm = TRUE), 4L)
           }
         }
       }
     }
-    if (length(HighSkew) > 0) {
-      a <-
-        tryCatch({
-          data.table::as.data.table(data.table::melt(HighSkew))
-        },
-        error = function(x)
-          NULL)
-      if (dim(a)[1] != 0) {
+    if(length(HighSkew) > 0L) {
+      a <- tryCatch({data.table::as.data.table(data.table::melt(HighSkew))}, error = function(x) NULL)
+      if(dim(a)[1L] != 0L) {
         data.table::setnames(a, c("L1", "value"), c("ColName", "HighSkewFeatures"))
-        data.table::setcolorder(a, c(2, 1))
+        data.table::setcolorder(a, c(2L, 1L))
         return(a)
       } else {
         return(NULL)
@@ -260,75 +167,51 @@ ProblematicFeatures <- function(data,
   
   # Initalize collection
   collect <- list()
-  z <- 0
+  z <- 0L
   
   # LowVarianceFeatures Run----
-  a <-
-    tryCatch({
-      LowVarianceFeatures(data, NearZeroVarThresh = NearZeroVarThresh)
-    },
-    error = function(x)
-      NULL)
-  if (!is.null(a)) {
-    z <- z + 1
+  a <- tryCatch({LowVarianceFeatures(data, NearZeroVarThresh = NearZeroVarThresh)}, error = function(x) NULL)
+  if(!is.null(a)) {
+    z <- z + 1L
     collect[[z]] <- a
   }
   
   # HighCardinalityFeatures Run----
-  b <-
-    tryCatch({
-      HighCardinalityFeatures(data, CharUniqThresh = CharUniqThresh)
-    },
-    error = function(x)
-      NULL)
-  if (!is.null(b)) {
-    z <- z + 1
+  b <- tryCatch({HighCardinalityFeatures(data, CharUniqThresh = CharUniqThresh)}, error = function(x) NULL)
+  if(!is.null(b)) {
+    z <- z + 1L
     collect[[z]] <- b
   }
   
   # HighMissingCountFeatures Run----
-  c <- tryCatch({
-    HighMissingCountFeatures(data, NA_Rate = NA_Rate)
-  },
-  error = function(x)
-    NULL)
-  if (!is.null(c)) {
-    z <- z + 1
+  c <- tryCatch({HighMissingCountFeatures(data, NA_Rate = NA_Rate)}, error = function(x) NULL)
+  if(!is.null(c)) {
+    z <- z + 1L
     collect[[z]] <- c
   }
   
   # HighZeroCountFeatures Run----
-  d <-
-    tryCatch({
-      HighZeroCountFeatures(data, Zero_Rate = Zero_Rate)
-    },
-    error = function(x)
-      NULL)
-  if (!is.null(d)) {
-    z <- z + 1
+  d <- tryCatch({HighZeroCountFeatures(data, Zero_Rate = Zero_Rate)}, error = function(x) NULL)
+  if(!is.null(d)) {
+    z <- z + 1L
     collect[[z]] <- d
   }
   
   # HighSkewFeatures Run----
-  e <-
-    tryCatch({
-      HighSkewFeatures(data, HighSkewThresh = HighSkewThresh)
-    },
-    error = function(x)
-      NULL)
-  if (!is.null(e)) {
-    z <- z + 1
+  e <- tryCatch({HighSkewFeatures(data, HighSkewThresh = HighSkewThresh)}, error = function(x) NULL)
+  if(!is.null(e)) {
+    z <- z + 1L
     collect[[z]] <- e
   }
   
   # Combine Outputs
-  if (length(collect) == 0) {
+  if(length(collect) == 0L) {
     return(NULL)
-  } else if (length(collect) == 1) {
-    return(collect[[1]])
+  } else if (length(collect) == 1L) {
+    return(collect[[1L]])
   } else {
-    for (x in seq_len(length(collect))) {
-      if (x == 1) {
+    for(x in seq_len(length(collect))) {
+      if(x == 1L) {
         val <- collect[[x]]
       } else {
         temp <- collect[[x]]
