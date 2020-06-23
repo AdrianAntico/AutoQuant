2e4b0eac519aa453717361c9ec5812959999daa3
Adrian <adrianantico@gmail.com>
2020-06-22 18:39:57 -0700

    Update AutoMLChainLadderForecasting.R

--- R/AutoMLChainLadderForecasting.R
+++ R/AutoMLChainLadderForecasting.R
@@ -359,8 +359,8 @@ AutoChainLadderForecast <- function(data,
     print("# Update forecast data----")
     temp1[ScoreRecords == 1, Rate := temp[which(Predictions < 0), Predictions := 0][[1L]]]
     temp1[ScoreRecords == 1, eval(ArgsList$ConversionMeasure) := Rate * (get(ArgsList$BaseFunnelMeasure) + 1)]
-    temp1 <- temp1[ScoreRecords == 1, .SD, .SDcols = c(eval(ArgsList$CalendarDate),eval(ArgsList$CohortDate),eval(ArgsList$CohortPeriodsVariable),eval(ArgsList$ModelID),eval(ArgsList$BaseFunnelMeasure),eval(ArgsList$ConversionMeasure),"Rate")]
-    data <- data.table::rbindlist(list(data[ScoreRecords != 1, .SD, .SDcols = c(eval(ArgsList$CalendarDate),eval(ArgsList$CohortDate),eval(ArgsList$CohortPeriodsVariable),eval(ArgsList$ModelID),eval(ArgsList$BaseFunnelMeasure),eval(ConversionMeasure),"Rate")], temp1), fill = TRUE, use.names = TRUE)
+    temp1 <- temp1[ScoreRecords == 1, .SD, .SDcols = c(eval(ArgsList$CalendarDate),eval(ArgsList$CohortDate),eval(ArgsList$CohortPeriodsVariable),SegmentName,eval(ArgsList$BaseFunnelMeasure),eval(ArgsList$ConversionMeasure),"Rate")]
+    data <- data.table::rbindlist(list(data[ScoreRecords != 1, .SD, .SDcols = c(eval(ArgsList$CalendarDate),eval(ArgsList$CohortDate),eval(ArgsList$CohortPeriodsVariable),SegmentName,eval(ArgsList$BaseFunnelMeasure),eval(ArgsList$ConversionMeasure),"Rate")], temp1), fill = TRUE, use.names = TRUE)
     
     # DE: Save forecasts to file----
     data.table::fwrite(data, file = file.path(normalizePath(ForecastOutput), paste0(ArgsList$ModelID, "_Forecasts.csv")))
