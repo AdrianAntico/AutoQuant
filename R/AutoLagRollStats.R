1ee1c93a4916231677f7e9ea02d6a0987949c97d
Adrian <adrianantico@gmail.com>
2020-07-01 16:53:42 -0700

    Update docs

--- R/AutoLagRollStats.R
+++ R/AutoLagRollStats.R
@@ -58,12 +58,12 @@
 #'   SimpleImpute         = TRUE,
 #'   
 #'   # Calculated Columns
-#'   Lags                  = list("days" = c(seq(1,5,1)), "weeks" = c(seq(1,3,1)), "months" = c(seq(1,2,1))),
-#'   MA_RollWindows        = list("days" = c(seq(1,5,1)), "weeks" = c(seq(1,3,1)), "months" = c(seq(1,2,1))),
-#'   SD_RollWindows        = NULL,
-#'   Skew_RollWindows      = NULL,
-#'   Kurt_RollWindows      = NULL,
-#'   Quantile_RollWindows  = NULL,
+#'   Lags                 = list("days" = c(seq(1,5,1)), "weeks" = c(seq(1,3,1)), "months" = c(seq(1,2,1))),
+#'   MA_RollWindows       = list("days" = c(seq(1,5,1)), "weeks" = c(seq(1,3,1)), "months" = c(seq(1,2,1))),
+#'   SD_RollWindows       = NULL,
+#'   Skew_RollWindows     = NULL,
+#'   Kurt_RollWindows     = NULL,
+#'   Quantile_RollWindows = NULL,
 #'   Quantiles_Selected   = c("q5","q10","q95"),
 #'   Debug                = FALSE)
 #' @export
