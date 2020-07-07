02c1af5ad0e815b5e7a4bf245f7113706814e020
Adrian <adrianantico@gmail.com>
2020-07-06 20:34:13 -0700

    Update function

--- R/AutoMLChainLadderTrainer.R
+++ R/AutoMLChainLadderTrainer.R
@@ -711,17 +711,15 @@ AutoCatBoostChainLadder <- function(data,
       if(proc %chin% c("evaluate","eval")) {
         Features <- names(TrainData)[!names(TrainData) %chin% c(eval(CalendarDate),eval(CohortDate),eval(BaseFunnelMeasure),eval(ConversionMeasure),eval(ConversionRateMeasure))]
         idcols <- names(TrainData)[!names(TrainData) %in% Features]
+        if(ModelID %chin% names(TrainData)) Features <- Features[!Features %chin% ModelID]
       } else {
         Features <- names(data)[!names(data) %chin% c(eval(CalendarDate),eval(CohortDate),eval(BaseFunnelMeasure),eval(ConversionMeasure),eval(ConversionRateMeasure))]
         idcols <- names(data)[!names(data) %in% Features]
+        if(ModelID %chin% names(data)) Features <- Features[!Features %chin% ModelID]
       }
-      
-      if(ModelID %chin% names(data)) Features <- Features[!Features %chin% ModelID]
       
       # Define number of trees----
-      if(proc %chin% c("eval","evaluation","evaluate")) {
-        NTrees <- Trees
-      }
+      if(proc %chin% c("eval","evaluation","evaluate")) NTrees <- Trees
       
       # Build model----
       x <- system.time(gcFirst = FALSE, TestModel <- RemixAutoML::AutoCatBoostRegression(
