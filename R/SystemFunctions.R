68d7783113b6ecf4a1b022dcdddd71d2ed9a23bd
Adrian <adrianantico@gmail.com>
2020-08-05 21:54:36 -0700

    Update docs

--- R/SystemFunctions.R
+++ R/SystemFunctions.R
@@ -22,74 +22,74 @@ PathNormalizer <- function(Path) {
 #' @param ExistsButNoProjectList Set to TRUE if the folder exists but not the ProjectList file
 #' @return Returns a proper path file string
 #' @export
-CreateProjectFolders <- function(ProjectName = input$ID_NewProjectName, 
+CreateProjectFolders <- function(ProjectName = input$ID_NewProjectName,
                                  RootPath = input$ID_Root_Folder,
                                  ExistsButNoProjectList = FALSE,
                                  Local = FALSE) {
-  
+
   # Create Collection List----
   ProjectList <- list()
-  
+
   # Store Project Name----
   ProjectList[["ProjectName"]] <- ProjectName
-  
+
   # Define Project Folder Root Path----
   if(Local) {
-    ProjectPath <- file.path(PathNormalizer(RootPath),ProjectName)  
+    ProjectPath <- file.path(PathNormalizer(RootPath),ProjectName)
   } else {
     ProjectPath <- paste0("./",ProjectName)
   }
-  
+
   ProjectList[["ProjectFolderPath"]] <- ProjectPath
-  
+
   # Create Project Folder----
   if(!ExistsButNoProjectList) {
-    
+
     # Create Data folder----
     DataPath <- file.path(ProjectPath,"Data")
-    dir.create(path =  DataPath, showWarnings = TRUE, recursive = TRUE)  
+    dir.create(path =  DataPath, showWarnings = TRUE, recursive = TRUE)
     ProjectList[["DataFolderPath"]] <- DataPath
-    
+
     # Create Models folder----
     ModelsPath <- file.path(ProjectPath,"Models")
     dir.create(path =  ModelsPath, showWarnings = TRUE, recursive = TRUE)
     ProjectList[["ModelsFolderPath"]] <- ModelsPath
-    
+
     # Create MetaData folder----
     MetaDataPath <- file.path(ProjectPath,"MetaData")
-    dir.create(path =  MetaDataPath, 
+    dir.create(path =  MetaDataPath,
                showWarnings = TRUE, recursive = TRUE)
     ProjectList[["MetaDataPath"]] <- MetaDataPath
-    
+
     # Save ProjectList to File----
     save(ProjectList, file = file.path(MetaDataPath,paste0(ProjectName,"_ProjectList.Rdata")))
-  
+
   } else {
-    
+
     # Create Data folder----
     DataPath <- file.path(ProjectPath,"Data")
     ProjectList[["DataFolderPath"]] <- DataPath
-    
+
     # Create Models folder----
     ModelsPath <- file.path(ProjectPath,"Models")
     ProjectList[["ModelsFolderPath"]] <- ModelsPath
-    
+
     # Create MetaData folder----
     MetaDataPath <- file.path(ProjectPath,"MetaData")
     ProjectList[["MetaDataPath"]] <- MetaDataPath
-    
+
     # Save ProjectList to File----
     save(ProjectList, file = file.path(MetaDataPath,paste0(ProjectName,"_ProjectList.Rdata")))
   }
-  
+
   # Return List to Use in Other Modules----
   return(ProjectList)
 }
 
 #' DownloadCSVFromStorageExplorer
-#' 
+#'
 #' DownloadCSVFromStorageExplorer
-#' 
+#'
 #' @author Adrian Antico
 #' @family Azure
 #' @param UploadCSVObjectName Name of the file you uploaded to the Microsoft Azure Storage Explorer
@@ -99,39 +99,39 @@ CreateProjectFolders <- function(ProjectName = input$ID_NewProjectName,
 #' @param DataStoreName The name of the store in data factory where you uploaded your data
 #' @export
 DownloadCSVFromStorageExplorer <- function(UploadCSVObjectName = 'TransfersRawData.csv',
-                                           SaveCSVFilePath = file.path(Root), 
+                                           SaveCSVFilePath = file.path(Root),
                                            SaveCSVName = "RawData.csv",
                                            UploadLocation = 'Analytics Sandbox/Machine Learning',
-                                           DataStoreName = "johnstestdatastore") {
-  
+                                           DataStoreName = NULL) {
+
   # Check if azuremlsdk is installed----
   if(!"azuremlsdk" %chin% installed.packages()) return("You need to run install.packages('azuremlsdk')")
-  
+
   # Options----
   options(warn = -1)
-  
+
   # Azure stuff----
   ws <- azuremlsdk::load_workspace_from_config()
   ds <- azuremlsdk::get_datastore(ws, DataStoreName)
-  
+
   # transfer files----
   dset <- azuremlsdk::create_tabular_dataset_from_delimited_files(
-    path = ds$path(file.path(UploadLocation, UploadCSVObjectName)), 
+    path = ds$path(file.path(UploadLocation, UploadCSVObjectName)),
     validate = TRUE,
-    include_path = FALSE, 
+    include_path = FALSE,
     infer_column_types = TRUE,
-    set_column_types = NULL, 
-    separator = ",", 
+    set_column_types = NULL,
+    separator = ",",
     header = TRUE,
     partition_format = NULL)
-  
+
   # Pull in data, convert it to data.table----
   data <- dset$to_pandas_dataframe()
   data <- data.table::as.data.table(data)
-  
+
   # Write data to file----
   data.table::fwrite(data, file = file.path(SaveCSVFilePath, SaveCSVName))
-  
+
   # Turn warnings back on----
   options(warn = 0)
 }
